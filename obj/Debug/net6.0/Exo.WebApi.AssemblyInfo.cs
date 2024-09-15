d2b1ebde733b9d1d00925e5f8ec1713cef5b3f36
Gessivanf <oliveiraferreira076@gmail.com>
2024-09-15 10:44:06 -0300

    atualizarA5

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c480b79f9a1624bc2cfaca2b711269cf661d9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e78b2d06ac6dba9f6f68f1ecd8365907abb4b36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
